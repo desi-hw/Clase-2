ecbe956401af99480023f3b0b72698073a60d05f
desi-hw <desireewasszabo@gmail.com>
2025-10-21 21:26:21 -0600

    Add basic number input and comparison in Ejercicio1.c
    Fix typo in scanf and improve prompt message.

---
+++ Ejercicio1.c
@@ -0,0 +1,13 @@
+/*Huitrón Was-Szabo Elizabeth Desireé
+Ejercicio 1*/
+#include<stdio.h>
+void main()
+{
+  int num;
+  printf ("Dame un número: ?n");
+  scantf("%d", &num);
+  if(num>100)
+  {
+    printf("El numero es mayor a 100");
+  }
+}
