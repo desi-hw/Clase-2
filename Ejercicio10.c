19bfa78388c01430bf2170d2652f01966488e74e
desi-hw <desireewasszabo@gmail.com>
2025-10-22 12:13:01 -0600

    Fix integer input and sorting logic
    Refactor the program to correctly read and sort three integers.

--- Ejercicio10.c
+++ Ejercicio10.c
@@ -3,5 +3,31 @@ Ejercicio 10*/
 #include<stdio.h>
 void main()
 {
-  float
+  int a,b,c;
+  printf("Dame 3 numeros separados por comas: \n");
+  scanf("%d,%d,%d", &a, &b, &c);
+  if(a>b)
+  {
+    if(b>c)
+      printf("%d %d %d", a,b,c);
+    else
+    {
+      if(a>c)
+        printf("%d %d %d", a,c,b);
+      else
+        printf("%d %d %d", c,a,b);
+    }
+  }
+  else
+  {
+    if(a>c)
+      printf("%d %d %d", b,a,c);
+    else
+    {
+      if(b>c)
+        printf("%d %d %d", b,c,a);
+      else
+        printf("%d %d %d", c,b,a);
+    }
+  }
 }
